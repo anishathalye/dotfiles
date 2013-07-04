f50c536d964f3192d1e14973bf15694f8151d60f
Anish Athalye <aathalye@me.com>
2013-07-04 14:53:19 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+dotfiles
+========
+
+Clone this repo and run `setup.sh` to configure your development environment.
---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Script to link all the files in dotfiles to the home directory
+
+BASEDIR=$(dirname $0)
+echo $BASEDIR
+
+# ln -s ${BASEDIR}/.conf.file ~

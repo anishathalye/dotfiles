204a2082225064f07a190171a681dc1b4236fc01
Anish Athalye <aathalye@me.com>
2013-09-12 15:48:18 -0400

    Update zsh to use vim style line editing

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -12,8 +12,8 @@ export SAVEHIST=$HISTSIZE
 # Use vim as the editor
 export EDITOR=vim
 
-# Use emacs mode in vim
-bindkey -e
+# Use vim style line editing in zsh
+bindkey -v
 
 # Use incremental search
 bindkey "^R" history-incremental-search-backward

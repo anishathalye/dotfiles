74d984d251e3adb7e01cacba6963fa8e9017ef43
Anish Athalye <aathalye@me.com>
2013-10-05 13:42:09 -0400

    Add settings for pip

---
+++ zsh/external.zsh
@@ -0,0 +1,5 @@
+# pip should only run if there is a virtualenv currently activated
+export PIP_REQUIRE_VIRTUALENV=true
+
+# Cache pip-installed packages to avoid re-downloading
+export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache

ec4cfbd2daab6425c84e0f61f7323654b49073e2
Anish Athalye <aathalye@me.com>
2014-03-18 22:04:42 -0400

    Separate install data from installer application

--- install.py
+++ install.py
@@ -2,58 +2,9 @@
 
 # Script to automatically set up the environment
 
-# link names are absolute (with '~' allowed, which will be expanded)
-# target names are relative with respect to this python script
-# target directories should have a trailing '/'
-links = {
-    # tmux
-    '~/.tmux.conf': 'tmux.conf',
-    
-    # screen
-    '~/.screenrc': 'screenrc',
+import sys, os, subprocess, json
 
-    # vim
-    '~/.vimrc': 'vimrc',
-    '~/.vim': 'vim/',
-
-    # zsh
-    '~/.zshrc': 'zshrc',
-    '~/.zsh': 'zsh/',
-
-    # git
-    '~/.gitconfig': 'gitconfig',
-    '~/.gitignore_global': 'gitignore_global',
-
-    # sbt
-    '~/.sbt': 'sbt/',
-
-    # rbenv
-    '~/.rbenv': 'rbenv/',
-
-    # rubygems
-    '~/.gemrc': 'gemrc',
-
-    # rtorrent
-    '~/.rtorrent.rc': 'rtorrent.rc',
-    '~/.rtorrent': 'rtorrent/',
-
-    # axel
-    '~/.axelrc': 'axelrc'
-}
-
-# shell commands (array of (msg, cmd))
-
-# shell commands to run before linking
-precmds = []
-
-# shell commands to run after linking
-postcmds = [
-    ('git update-submodules', 'Installing/updating submodules')
-]
-
-####################
-
-import sys, os, subprocess
+INSTALL_DATA_FILE = 'install.json'
 
 # colors
 class colors:
@@ -118,7 +69,7 @@ def link(source, link_name):
         INFO('[ ] link exists %s -> %s' % (link_name, source))
     return unsuccessful
 
-def process_links():
+def process_links(links):
     unsuccessful = False
     for dest, source in links.items():
         if link(source, dest):
@@ -151,10 +102,16 @@ def process_shell(cmds):
         SUCC('SUCCESS: all tasks executed')
 
 def main():
+    data_file_path = os.path.join(self_path(), INSTALL_DATA_FILE)
+    with open(data_file_path) as data_file:
+        data = json.load(data_file)
+        links = data['links']
+        precmds = data['precmds']
+        postcmds = data['postcmds']
     pre_fail = process_shell(precmds)
     if precmds:
         NEWLINE()
-    link_fail = process_links()
+    link_fail = process_links(links)
     if postcmds:
         NEWLINE()
     post_fail = process_shell(postcmds)

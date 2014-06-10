4c056ca1a95c1c605baa9e86db17f2804423312a
Anish Athalye <me@anishathalye.com>
2014-06-10 15:39:36 -0700

    Improve zsh vim style line editing

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -14,6 +14,15 @@ export EDITOR=vim
 
 # Use vim style line editing in zsh
 bindkey -v
+# Movement
+bindkey -a 'gg' beginning-of-buffer-or-history
+bindkey -a 'G' end-of-buffer-or-history
+# Undo
+bindkey -a 'u' undo
+bindkey -a '^R' redo
+# Backspace
+bindkey '^?' backward-delete-char
+bindkey '^H' backward-delete-char
 
 # Use incremental search
 bindkey "^R" history-incremental-search-backward

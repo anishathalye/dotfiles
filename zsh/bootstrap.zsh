b2f08055334bd2e6fb4114e1d5c1f3c809beeaaf
Anish Athalye <me@anishathalye.com>
2014-06-19 17:29:54 -0700

    Remove rbenv related configuration

--- zsh/bootstrap.zsh
+++ zsh/bootstrap.zsh
@@ -1,8 +1,3 @@
 if [[ "$(tput colors)" == "256" ]]; then
     eval $(dircolors ~/.zsh/plugins/dircolors-solarized/dircolors.256dark)
 fi
-
-# if rbenv is installed (and not an alias)
-if rbenv_loc="$(type -p 'rbenv')" && ! [ -z "${rbenv_loc}" ]; then
-    eval "$(rbenv init -)"
-fi

4c0618dfd8af8febd0d5646493322bfb7df375e0
Anish Athalye <me@anishathalye.com>
2014-07-09 16:08:43 -0700

    Improve zsh git prompt
    Increase readability of prompt by adding spaces between elements.

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -126,7 +126,7 @@ GIT_PROMPT_PREFIX="%{$fg[violet]%}%B(%b%{$reset_color%}"
 GIT_PROMPT_SUFFIX="%{$fg[violet]%}%B)%b%{$reset_color%}"
 GIT_PROMPT_AHEAD="%{$fg[teal]%}%B+NUM%b%{$reset_color%}"
 GIT_PROMPT_BEHIND="%{$fg[orange]%}%B-NUM%b%{$reset_color%}"
-GIT_PROMPT_MERGING="%{$fg[orange]%}%Bx%b%{$reset_color%}"
+GIT_PROMPT_MERGING="%{$fg[cyan]%}%Bx%b%{$reset_color%}"
 GIT_PROMPT_UNTRACKED="%{$fg[red]%}%B-%b%{$reset_color%}"
 GIT_PROMPT_MODIFIED="%{$fg[yellow]%}%B-%b%{$reset_color%}"
 GIT_PROMPT_STAGED="%{$fg[green]%}%B-%b%{$reset_color%}"
@@ -155,25 +155,36 @@ function parse_git_state() {
 
     local NUM_BEHIND="$(git log --oneline ..@{u} 2> /dev/null | wc -l | tr -d ' ')"
     if [ "$NUM_BEHIND" -gt 0 ]; then
+        if [[ -n $GIT_STATE ]]; then
+            GIT_STATE="$GIT_STATE "
+        fi
     GIT_STATE=$GIT_STATE${GIT_PROMPT_BEHIND//NUM/$NUM_BEHIND}
     fi
 
     local GIT_DIR="$(git rev-parse --git-dir 2> /dev/null)"
     if [ -n $GIT_DIR ] && test -r $GIT_DIR/MERGE_HEAD; then
+        if [[ -n $GIT_STATE ]]; then
+            GIT_STATE="$GIT_STATE "
+        fi
     GIT_STATE=$GIT_STATE$GIT_PROMPT_MERGING
     fi
 
     if [[ -n $(git ls-files --other --exclude-standard 2> /dev/null) ]]; then
-    GIT_STATE=$GIT_STATE$GIT_PROMPT_UNTRACKED
+    GIT_DIFF=$GIT_PROMPT_UNTRACKED
     fi
 
     if ! git diff --quiet 2> /dev/null; then
-    GIT_STATE=$GIT_STATE$GIT_PROMPT_MODIFIED
+    GIT_DIFF=$GIT_DIFF$GIT_PROMPT_MODIFIED
     fi
 
     if ! git diff --cached --quiet 2> /dev/null; then
-    GIT_STATE=$GIT_STATE$GIT_PROMPT_STAGED
+    GIT_DIFF=$GIT_DIFF$GIT_PROMPT_STAGED
     fi
+
+    if [[ -n $GIT_STATE && -n $GIT_DIFF ]]; then
+        GIT_STATE="$GIT_STATE "
+    fi
+    GIT_STATE="$GIT_STATE$GIT_DIFF"
 
     if [[ -n $GIT_STATE ]]; then
     echo "$GIT_PROMPT_PREFIX$GIT_STATE$GIT_PROMPT_SUFFIX"

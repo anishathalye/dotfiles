d87db93891947b2af32e3cef2e0fecf95d42b820
Anish Athalye <me@anishathalye.com>
2014-10-25 09:46:46 -0400

    Improve zsh 'up' alias
    Rewrite the alias to compute the target directory and then use a single
    `cd` comand to change directories. This makes it so that `cd -` commands
    work properly to switch back to the previous directory. In addition,
    running a command like `up 1000` will not take forever because of an
    early escape condition that triggers when the root of the directory tree
    is reached.

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -50,17 +50,24 @@ fi
 # Go up [n] directories
 function up()
 {
+    local cdir="$(pwd)"
     if [[ "${1}" == "" ]]; then
-        cd ..
+        cdir="$(dirname "${cdir}")"
     elif ! [[ "${1}" =~ ^[0-9]+$ ]]; then
         echo "Error: argument must be a number"
     elif ! [[ "${1}" -gt "0" ]]; then
         echo "Error: argument must be positive"
     else
         for i in {1..${1}}; do
-            cd ..
+            local ncdir="$(dirname "${cdir}")"
+            if [[ "${cdir}" == "${ncdir}" ]]; then
+                break
+            else
+                cdir="${ncdir}"
+            fi
         done
     fi
+    cd "${cdir}"
 }
 
 # Execute a command in a specific directory

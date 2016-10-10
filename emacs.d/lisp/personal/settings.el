6ad40f3215c6593b940028a5b3774c977c97c025
Anish Athalye <me@anishathalye.com>
2016-10-10 13:19:38 -0400

    Improve scrolling in emacs

--- emacs.d/lisp/personal/settings.el
+++ emacs.d/lisp/personal/settings.el
@@ -2,6 +2,13 @@
 
 (menu-bar-mode -1) ; disable menu bar
 
+;; improve scrolling
+(setf scroll-margin 5
+      scroll-step 1
+      scroll-conservatively 10000
+      scroll-up-aggressively 0.01
+      scroll-down-aggressively 0.01)
+
 (show-paren-mode 1) ; highlight matching parens
 
 (global-hl-line-mode 1) ; highlight current line

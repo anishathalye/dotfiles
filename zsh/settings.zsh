7237d28d415fbbb39e58827d7818b80f1c585710
Anish Athalye <me@anishathalye.com>
2017-01-15 11:07:21 -0500

    Use vim style navigation keys in menu completion

--- zsh/settings.zsh
+++ zsh/settings.zsh
@@ -1,6 +1,12 @@
 # Initialize completion
 autoload -Uz compinit && compinit -i
 zstyle ':completion:*' menu select=4
+zmodload zsh/complist
+# Use vim style navigation keys in menu completion
+bindkey -M menuselect 'h' vi-backward-char
+bindkey -M menuselect 'k' vi-up-line-or-history
+bindkey -M menuselect 'l' vi-forward-char
+bindkey -M menuselect 'j' vi-down-line-or-history
 
 # Initialize editing command line
 autoload -U edit-command-line && zle -N edit-command-line

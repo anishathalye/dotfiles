40b0e088c3f349bffe6cd343def5723c2272e920
Anish Athalye <me@anishathalye.com>
2017-05-01 22:19:37 -0400

    Add autocompletion for Python shell

---
+++ pythonrc
@@ -0,0 +1,2 @@
+import rlcompleter, readline
+readline.parse_and_bind('tab:complete')
--- shell/external.sh
+++ shell/external.sh
@@ -3,3 +3,6 @@ export PIP_REQUIRE_VIRTUALENV=true
 
 # Cache pip-installed packages to avoid re-downloading
 export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
+
+# Python startup file
+export PYTHONSTARTUP=$HOME/.pythonrc

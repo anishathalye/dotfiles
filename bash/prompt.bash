2d30b38d5a56e9df0b6a791bc717a6c8b8be12b8
Anish Athalye <me@anishathalye.com>
2020-07-25 15:54:54 -0400

    Add simple Bash prompt for recording demos

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -19,3 +19,12 @@ machine_name() {
 PROMPT_DIRTRIM=3
 PS1="\n${COLOR_BLUE}#${COLOR_DEFAULT} ${COLOR_CYAN}\\u${COLOR_DEFAULT} ${COLOR_GREEN}at${COLOR_DEFAULT} ${COLOR_MAGENTA}\$(machine_name)${COLOR_DEFAULT} ${COLOR_GREEN}in${COLOR_DEFAULT} ${COLOR_YELLOW}\w${COLOR_DEFAULT}\n\$(if [ \$? -ne 0 ]; then echo \"${COLOR_RED}!${COLOR_DEFAULT} \"; fi)${COLOR_BLUE}>${COLOR_DEFAULT} "
 PS2="${COLOR_BLUE}>${COLOR_DEFAULT} "
+
+COLOR_GRAY='\e[38;5;246m'
+COLOR_LIGHTBLUE='\e[38;5;33m'
+
+demoprompt() {
+    PROMPT_DIRTRIM=1
+    PS1="${COLOR_GRAY}\w ${COLOR_BLUE}\$ "
+    trap '[[ -t 1 ]] && tput sgr0' DEBUG
+}

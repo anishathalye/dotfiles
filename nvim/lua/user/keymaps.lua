efab2b91e2c8dc617c01d81104920e03c20a55ff
ravenxrz <zhang.xingrui@foxmail.com>
2022-03-13 09:46:47 +0800

    add some plugins

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -31,6 +31,7 @@ keymap("n", "<leader>l", ":nohl<cr>", opts)
 -- save buffer
 keymap("n", "<leader>w", ":w<cr>", opts)
 -- quite buffer
+-- TODO: fix this
 keymap("n", "<leader>q", ":quit<cr>", opts)
 
 
@@ -74,13 +75,15 @@ keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 
 -- Terminal --
 -- Better terminal navigation
-keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
-keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
-keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
-keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
+-- keymap("t", "<C-h>", "<C-\\><C-N><C-w>h", term_opts)
+-- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
+-- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
+-- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
 
 
 
 -- keymap("n", "<leader>f", "<cmd>Telescope find_files<cr>", opts)
 keymap("n", "<Space>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
 keymap("n", "<Space>F", "<cmd>Telescope live_grep<cr>", opts)
+keymap("n", "<leader>u", "<cmd>Telescope lsp_references<cr>", opts)
+
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -56,6 +56,10 @@ return packer.startup(function(use)
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
   use "hrsh7th/cmp-nvim-lsp"
   use "hrsh7th/cmp-nvim-lua"
+  -- toggle terminal
+  use "akinsho/toggleterm.nvim"
+  -- project manager
+  use "ahmedkhalf/project.nvim"
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine

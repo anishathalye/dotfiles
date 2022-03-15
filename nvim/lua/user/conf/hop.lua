77b6e38de0e7e9d2d2ef252460256c4256bcce91
ravenxrz <zhang.xingrui@foxmail.com>
2022-03-15 22:23:09 +0800

    add hop plugin

--- nvim/lua/user/conf/hop.lua
+++ nvim/lua/user/conf/hop.lua
@@ -1,9 +1,10 @@
 -- https://github.com/phaazon/hop.nvim
-local status_ok, gitsigns = pcall(require, "hop")
+local status_ok, hop = pcall(require, "hop")
 if not status_ok then
   vim.notify("hop not found!")
   return
 end
+hop.setup()
 
 -- place this in one of your configuration file(s)
 -- enhance f motion
@@ -15,7 +16,7 @@ vim.api.nvim_set_keymap('', 't', "<cmd>lua require'hop'.hint_char1({ direction =
 vim.api.nvim_set_keymap('', 'T', "<cmd>lua require'hop'.hint_char1({ direction = require'hop.hint'.HintDirection.BEFORE_CURSOR, current_line_only = true })<cr>", {})
 
 -- place this in one of your configuration file(s)
-vim.api.nvim_set_keymap('n', '<leader>hw', "<cmd>HopWord<cr>", {})
-vim.api.nvim_set_keymap('n', '<leader>hj', "<cmd>HopLine<cr>", {})
-vim.api.nvim_set_keymap('n', '<leader>hk', "<cmd>HopLine<cr>", {})
-vim.api.nvim_set_keymap('n', '<leader>hs', "<cmd>HopChar1<cr>", {})
+vim.api.nvim_set_keymap('n', '<leader><leader>w', "<cmd>HopWord<cr>", {})
+vim.api.nvim_set_keymap('n', '<leader><leader>j', "<cmd>HopLine<cr>", {})
+vim.api.nvim_set_keymap('n', '<leader><leader>k', "<cmd>HopLine<cr>", {})
+vim.api.nvim_set_keymap('n', '<leader><leader>s', "<cmd>HopChar1<cr>", {})

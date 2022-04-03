6b4fb7b17e3f1ecb56691bbc7050629e9bbe9c0c
ravenxrz <zhang.xingrui@foxmail.com>
2022-04-03 09:44:48 +0800

    add notify plugin

--- nvim/lua/user/conf/init.lua
+++ nvim/lua/user/conf/init.lua
@@ -1,3 +1,4 @@
+require "user.conf.notify"
 require "user.conf.nvim-tree"
 require "user.conf.cmp"
 require "user.conf.telescope"
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -79,6 +79,7 @@ return packer.startup(function(use)
   use "wbthomason/packer.nvim" -- Have packer manage itself
   use "nvim-lua/popup.nvim" -- An implementation of the Popup API from vim in Neovim
   use "nvim-lua/plenary.nvim" -- Useful lua functions used ny lots of plugins
+  use "rcarriga/nvim-notify"  -- notify
 
   -- Editor enhace
   use "windwp/nvim-autopairs" -- Autopairs, integrates with both cmp and treesitter

1c4f4190defa4c4a4686d06611cf002227d5aa4c
ravenxrz <zhang.xingrui@foxmail.com>
2022-04-09 11:30:46 +0800

    change the default theme to darkplus(vscode like)

--- nvim/lua/user/colorscheme.lua
+++ nvim/lua/user/colorscheme.lua
@@ -1,7 +1,11 @@
-local colorscheme = "onedark"
+local colorscheme = "darkplus"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
   vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
+end
+
+if colorscheme == "onedark" then
+  require "user.conf.onedark"
 end

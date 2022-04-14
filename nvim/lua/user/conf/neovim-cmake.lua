0040852ea1768166c726fae384f224a7fb4ddaa8
ravenxrz <zhang.xingrui@foxmail.com>
2022-04-14 12:53:33 +0800

    change neovim-cmake dap config to support pretty printing

--- nvim/lua/user/conf/neovim-cmake.lua
+++ nvim/lua/user/conf/neovim-cmake.lua
@@ -30,11 +30,20 @@ require('cmake').setup({
   on_build_output = nil, -- Callback which will be called on every line that is printed during build process. Accepts printed line as argument.
   quickfix_height = 10, -- Height of the opened quickfix.
   quickfix_only_on_error = false, -- Open quickfix window only if target build failed.
-  dap_configuration = { 
-    type = 'cppdbg', 
-    request = 'launch',
-    runInTerminal = true,
+  dap_configuration = {
+    name = "Launch file",
+    type = "cppdbg",
+    request = "launch",
+    cwd = '${workspaceFolder}',
     stopOnEntry = false,
+    runInTerminal = true,
+    setupCommands = {
+      {
+        description = 'enable pretty printing',
+        text = '-enable-pretty-printing',
+        ignoreFailures = false
+      },
+    },
   }, -- DAP configuration. By default configured to work with `lldb-vscode`.
   dap_open_command = require('dap').repl.open, -- Command to run after starting DAP session. You can set it to `false` if you don't want to open anything or `require('dapui').open` if you are using https://github.com/rcarriga/nvim-dap-ui
 })

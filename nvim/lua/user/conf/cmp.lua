8d935c4c5e21880569c0b77d11b8a4f8bb1e4f96
ravenxrz <zhang.xingrui@foxmail.com>
2022-04-14 10:17:59 +0800

    update cmp config

--- nvim/lua/user/conf/cmp.lua
+++ nvim/lua/user/conf/cmp.lua
@@ -126,8 +126,10 @@ cmp.setup {
     behavior = cmp.ConfirmBehavior.Replace,
     select = false,
   },
-  documentation = {
-    border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+  window = {
+    documentation = {
+      border = { "╭", "─", "╮", "│", "╯", "─", "╰", "│" },
+    },
   },
   experimental = {
     ghost_text = false,

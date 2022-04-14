1328c49412cde12315c80ea4fec106c024e20a38
ravenxrz <zhang.xingrui@foxmail.com>
2022-04-14 17:59:36 +0800

    change progress chars of lualine

--- nvim/lua/user/conf/lualine.lua
+++ nvim/lua/user/conf/lualine.lua
@@ -53,7 +53,8 @@ local location = {
 local progress = function()
 	local current_line = vim.fn.line(".")
 	local total_lines = vim.fn.line("$")
-	local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+	-- local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+  local chars = { "██", "▇▇", "▆▆", "▅▅", "▄▄", "▃▃", "▂▂", "▁▁", " ", }
 	local line_ratio = current_line / total_lines
 	local index = math.ceil(line_ratio * #chars)
 	return chars[index]

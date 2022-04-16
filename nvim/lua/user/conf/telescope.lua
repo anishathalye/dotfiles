c67cbb12b2a0f0eb051c520a30bf0b6d8e757d82
ravenxrz <zhang.xingrui@foxmail.com>
2022-04-16 11:03:50 +0800

    add rg exmaples in telescope comment

--- nvim/lua/user/conf/telescope.lua
+++ nvim/lua/user/conf/telescope.lua
@@ -10,6 +10,27 @@
 -- -g 通配符文件或文件夹，可以用!来取反
 -- -F fixed-string 原意字符串，类似python的 r'xxx'
 
+-- examples:
+-- command	Description
+-- rg image utils.py	Search in a single file utils.py
+-- rg image src/	Search in dir src/ recursively
+-- rg image	Search image in current dir recursively
+-- rg '^We' test.txt	Regex searching support (lines starting with We)
+-- rg -i image	Search image and ignore case (case-insensitive search)
+-- rg -s image	Smart case search
+-- rg -F '(test)'	Search literally, i.e., without using regular expression
+-- rg image -g '*.py'	File globing (search in certain files), can be used multiple times
+-- rg image -g '!*.py'	Negative file globing (do not search in certain files)
+-- rg image --type py or rg image -tpy1	Search image in Python file
+-- rg image -Tpy	Do not search image in Python file type
+-- rg -l image	Only show files containing image (Do not show the lines)
+-- rg --files-without-match image	Show files not containing image
+-- rg -v image	Inverse search (search files not containing image)
+-- rg -w image	Search complete word
+-- rg --count	Show the number of matching lines in a file
+-- rg --count-matches	Show the number of matchings in a file
+-- rg neovim --stats	Show the searching stat (how many matches, how many files searched etc.)
+
 
 
 local status_ok, telescope = pcall(require, "telescope")

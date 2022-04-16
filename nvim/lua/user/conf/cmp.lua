c97cc1f9a5436a5177d254f636b227066a720d4c
ravenxrz <zhang.xingrui@foxmail.com>
2022-04-16 20:16:53 +0800

    tweak settings

--- nvim/lua/user/conf/cmp.lua
+++ nvim/lua/user/conf/cmp.lua
@@ -70,22 +70,22 @@ cmp.setup {
     -- Accept currently selected item. If none selected, `select` first item.
     -- Set `select` to `false` to only confirm explicitly selected items.
     ["<CR>"] = cmp.mapping.confirm { select = true },
-    ["<Tab>"] = cmp.mapping(function(fallback)
-      if cmp.visible() then
-        cmp.select_next_item()
-      elseif luasnip.expandable() then
-        luasnip.expand()
-      elseif luasnip.expand_or_jumpable() then
-        luasnip.expand_or_jump()
-      elseif check_backspace() then
-        fallback()
-      else
-        fallback()
-      end
-    end, {
-      "i",
-      "s",
-    }),
+    -- ["<Tab>"] = cmp.mapping(function(fallback)
+    --   if cmp.visible() then
+    --     cmp.select_next_item()
+    --   elseif luasnip.expandable() then
+    --     luasnip.expand()
+    --   elseif luasnip.expand_or_jumpable() then
+    --     luasnip.expand_or_jump()
+    --   elseif check_backspace() then
+    --     fallback()
+    --   else
+    --     fallback()
+    --   end
+    -- end, {
+    --   "i",
+    --   "s",
+    -- }),
     ["<S-Tab>"] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_prev_item()
--- nvim/lua/user/conf/nvim_context_vt.lua
+++ nvim/lua/user/conf/nvim_context_vt.lua
@@ -7,7 +7,7 @@ end
 nvim_context_vt.setup({
   -- Enable by default. You can disable and use :NvimContextVtToggle to maually enable.
   -- Default: true
-  enabled = true,
+  enabled = false,
 
   -- Override default virtual text prefix
   -- Default: '-->'

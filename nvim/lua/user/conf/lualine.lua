e9125598640c691732109bf45ac91455e83e0057
ravenxrz <zhang.xingrui@foxmail.com>
2022-04-23 10:21:31 +0800

    lualine shows relative file path

--- nvim/lua/user/conf/lualine.lua
+++ nvim/lua/user/conf/lualine.lua
@@ -1,101 +1,114 @@
 local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
   vim.notify("lualine not found!")
-	return
+  return
 end
 
 local hide_in_width = function()
-	return vim.fn.winwidth(0) > 80
+  return vim.fn.winwidth(0) > 80
 end
 
 local diagnostics = {
-	"diagnostics",
-	sources = { "nvim_diagnostic" },
-	sections = { "error", "warn" },
-	symbols = { error = " ", warn = " " },
-	colored = false,
-	update_in_insert = false,
-	always_visible = true,
+  "diagnostics",
+  sources = { "nvim_diagnostic" },
+  sections = { "error", "warn" },
+  symbols = { error = " ", warn = " " },
+  colored = false,
+  update_in_insert = false,
+  always_visible = true,
 }
 
 local diff = {
-	"diff",
-	colored = false,
-	symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
+  "diff",
+  colored = false,
+  symbols = { added = " ", modified = " ", removed = " " }, -- changes diff symbols
   cond = hide_in_width
 }
 
 local mode = {
-	"mode",
-	fmt = function(str)
-		return "-- " .. str .. " --"
-	end,
+  "mode",
+  fmt = function(str)
+    return "-- " .. str .. " --"
+  end,
+}
+
+
+local file_name = {
+  'filename',
+  file_status = true, -- Displays file status (readonly status, modified status)
+  path = 1, -- 0: Just the filename
+  -- 1: Relative path
+  -- 2: Absolute path
+
+  shorting_target = 40, -- Shortens path to leave 40 spaces in the window
+  -- for other components. (terrible name, any suggestions?)
+  symbols = {
+    modified = '[+]', -- Text to show when the file is modified.
+    readonly = '[-]', -- Text to show when the file is non-modifiable or readonly.
+    unnamed = '[No Name]', -- Text to show for unnamed buffers.
+  },
 }
 
 local filetype = {
-	"filetype",
-	icons_enabled = false,
-	icon = nil,
+  "filetype",
+  icons_enabled = false,
+  icon = nil,
 }
 
 local branch = {
-	"branch",
-	icons_enabled = true,
-	icon = "",
+  "branch",
+  icons_enabled = true,
+  icon = "",
 }
 
 local location = {
-	"location",
-	padding = 0,
+  "location",
+  padding = 0,
 }
 
 -- cool function for progress
 local progress = function()
-	local current_line = vim.fn.line(".")
-	local total_lines = vim.fn.line("$")
-	-- local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
+  local current_line = vim.fn.line(".")
+  local total_lines = vim.fn.line("$")
+  -- local chars = { "__", "▁▁", "▂▂", "▃▃", "▄▄", "▅▅", "▆▆", "▇▇", "██" }
   local chars = { "██", "▇▇", "▆▆", "▅▅", "▄▄", "▃▃", "▂▂", "▁▁", " ", }
-	local line_ratio = current_line / total_lines
-	local index = math.ceil(line_ratio * #chars)
-	return chars[index]
+  local line_ratio = current_line / total_lines
+  local index = math.ceil(line_ratio * #chars)
+  return chars[index]
 end
 
 local spaces = function()
-	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+  return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
 end
-
 
 -- add gps module to get the position information
 -- local gps = require("nvim-gps")
 
 lualine.setup({
-	options = {
-		icons_enabled = true,
-		theme = "auto",
-		component_separators = { left = "", right = "" },
-		section_separators = { left = "", right = "" },
-    disabled_filetypes = {"alpha", "dashboard", "NvimTree", "Outline" },
-		always_divide_middle = true,
-	},
-	sections = {
-		lualine_a = { branch, diagnostics },
-		lualine_b = { mode },
-		-- lualine_c = {
-		--         { gps.get_location, cond = gps.is_available },
-		--     },
-		-- lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_x = { diff, spaces, "encoding", filetype },
-		lualine_y = { location },
-		lualine_z = { progress },
-	},
-	inactive_sections = {
-		lualine_a = {},
-		lualine_b = {},
-		lualine_c = { "filename" },
-		lualine_x = { "location" },
-		lualine_y = {},
-		lualine_z = {},
-	},
-	tabline = {},
-	extensions = {},
+  options = {
+    icons_enabled = true,
+    theme = "auto",
+    component_separators = { left = "", right = "" },
+    section_separators = { left = "", right = "" },
+    disabled_filetypes = { "alpha", "dashboard", "NvimTree", "Outline" },
+    always_divide_middle = true,
+  },
+  sections = {
+    lualine_a = { branch, diagnostics },
+    lualine_b = { mode },
+    lualine_c = { file_name },
+    lualine_x = { diff, spaces, "encoding", filetype, "fileformat" },
+    lualine_y = { location },
+    lualine_z = { progress },
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = { file_name },
+    lualine_x = { "location" },
+    lualine_y = {},
+    lualine_z = {},
+  },
+  tabline = {},
+  extensions = {},
 })

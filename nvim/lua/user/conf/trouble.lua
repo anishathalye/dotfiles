7eba1feddf345d2e7a12bcea987d475f82891aac
ravenxrz <zhang.xingrui@foxmail.com>
2022-04-23 18:16:42 +0800

    change trouble position to bottom

--- nvim/lua/user/conf/trouble.lua
+++ nvim/lua/user/conf/trouble.lua
@@ -5,7 +5,7 @@ if not status_ok then
 end
 
 trouble.setup({
-		position = "right", -- position of the list can be: bottom, top, left, right
+		position = "bottom", -- position of the list can be: bottom, top, left, right
 		height = 10, -- height of the trouble list when position is top or bottom
 		width = 50, -- width of the list when position is left or right
 		icons = true, -- use devicons for filenames

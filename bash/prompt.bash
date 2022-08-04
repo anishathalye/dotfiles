c0b6a8168158a9cdcfc8047da629658aaf174880
Matt Tran <34324060+matt2ology@users.noreply.github.com>
2022-08-03 22:55:42 -0700

    Organized prompt.bash
    Leave the campsite cleaner than when you found it...

--- bash/prompt.bash
+++ bash/prompt.bash
@@ -1,12 +1,13 @@
 ATTRIBUTE_BOLD='\[\e[1m\]'
 ATTRIBUTE_RESET='\[\e[0m\]'
 COLOR_DEFAULT='\[\e[39m\]'
-COLOR_RED='\[\e[31m\]'
-COLOR_GREEN='\[\e[32m\]'
-COLOR_YELLOW='\[\e[33m\]'
 COLOR_BLUE='\[\e[34m\]'
-COLOR_MAGENTA='\[\e[35m\]'
 COLOR_CYAN='\[\e[36m\]'
+COLOR_GRAY='\e[38;5;246m'
+COLOR_GREEN='\[\e[32m\]'
+COLOR_MAGENTA='\[\e[35m\]'
+COLOR_RED='\[\e[31m\]'
+COLOR_YELLOW='\[\e[33m\]'
 
 machine_name() {
     if [[ -f $HOME/.name ]]; then
@@ -19,8 +20,6 @@ machine_name() {
 PROMPT_DIRTRIM=3
 PS1="\n${COLOR_BLUE}#${COLOR_DEFAULT} ${COLOR_CYAN}\\u${COLOR_DEFAULT} ${COLOR_GREEN}at${COLOR_DEFAULT} ${COLOR_MAGENTA}\$(machine_name)${COLOR_DEFAULT} ${COLOR_GREEN}in${COLOR_DEFAULT} ${COLOR_YELLOW}\w${COLOR_DEFAULT}\n\$(if [ \$? -ne 0 ]; then echo \"${COLOR_RED}!${COLOR_DEFAULT} \"; fi)${COLOR_BLUE}>${COLOR_DEFAULT} "
 PS2="${COLOR_BLUE}>${COLOR_DEFAULT} "
-
-COLOR_GRAY='\e[38;5;246m'
 
 demoprompt() {
     PROMPT_DIRTRIM=1
